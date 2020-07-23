082edc2daf208ff6a3a3654cd78fb8d6bf212952
supergillie <peter.krooks@gmail.com>
2020-07-23 09:45:32 +0200

    lista klar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,12 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -8,21 +14,95 @@ public class Main {
     public static void main(String[] args) {
 
         // 1 sug upp texten alla rader
-        List<String> rows = Arrays.asList("adf");
+        List<String> rows = null;
+        try {
+            rows = Files.readAllLines(new File("resources/movie.txt").toPath(), Charset.defaultCharset() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printWithInfo(rows, "ListaInnanJustering");
 
-        System.out.println("Originalfile:");
+        // 2 ta bort tomma rader, rader som startar med en siffra,  <i>... och <i>
+        removeCrapRowsFromList(rows);
+        System.out.println("efter remove: " + rows.size());
+        printWithInfo(rows, "ListaInnanJustering");
 
+
+        // 3 remove crap from beginning of rows
+        removeStartCrap(rows);
+        printWithInfo(rows, "Lista efter crap tagits bort");
+
+        // multiplewordsrows to separate rows
+        List<String> oneWordList = new ArrayList<String>();
+        oneWordList.add("apansson");
+        oneWordList.add("apansson");
+        oneWordList.add("apansson");
+
+
+        oneWordList = separateLinesToWords(rows);
+        printWithInfo(oneWordList, "lista med ett ord på varje rad");
+
+
+        // save first song oak_cleaf_cluster.txt
+        // for us nothing changes
+
+    }
+
+    private static List<String> separateLinesToWords(List<String> rows) {
+        List<String> separated = new ArrayList<>();
+        try {
+            for(String s : rows)
+            {
+                String[] arr = s.split(" ");
+                for ( String ss : arr) {
+                    separated.add(ss
+                            .replace("</i>","")
+                            .replace("?","")
+                            .replace(".","")
+                            .replace(",","")
+
+
+                    );
+                }
+            }
+        } catch (Exception e) {
+        }
+        return(separated);
+    }
+
+    private static void removeStartCrap(List<String> rows) {
+        for (int i = 0; i < rows.size(); i++) {
+            if ((rows.get(i).startsWith("- ")) || (rows.get(i).startsWith("..."))) {
+                System.out.println("crap detected: " + rows.get(i));
+               // rows.set(i, rows.get(i).replaceAll("...", ""));
+                rows.set(i, rows.get(i).replaceAll("- ", ""));
+               // rows.set(i, rows.get(i).replaceAll(".", ""));
+                //rows.set(i, rows.get(i).replaceAll("</i>", ""));
+            }
+        }
+    }
+
+    private static void removeCrapRowsFromList(List<String> rows) {
+
+        for(int i = 0; i<rows.size(); i++){
+            if ((rows.get(i).length() == 0)
+                    || rows.get(i).startsWith("<")
+                    || Character.isDigit(rows.get(i).charAt(0)))
+            {
+               System.out.println(" rows.remove(value); " + rows.get(i));
+               rows.remove(i);
+               i--;
+            }
+        }
+
+    }
+
+    private static void printWithInfo(List<String> rows, String info) {
+        System.out.println(info + " beginning.");
         for(String s : rows){
             System.out.println(s);
         }
 
-        // 2 ta bort tomma rader
-
-        // 3 ta bort rader som startar med en siffra
-
-        // 4 ta bort <i>... och <i>
-
-        // save first song oak_cleaf_cluster.txt
-
+        System.out.println(info + " end. Number of rows: " + rows.size());
     }
 }
