f6687e0657186c7544834c54d816687f3a86a012
supergillie <peter.krooks@gmail.com>
2020-07-23 07:14:53 +0200

    lista

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // 1 sug upp texten alla rader
+        List<String> rows = Arrays.asList("adf");
+
+        System.out.println("Originalfile:");
+
+        for(String s : rows){
+            System.out.println(s);
+        }
+
+        // 2 ta bort tomma rader
+
+        // 3 ta bort rader som startar med en siffra
+
+        // 4 ta bort <i>... och <i>
+
+        // save first song oak_cleaf_cluster.txt
+
     }
 }
